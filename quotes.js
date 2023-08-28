184feecf9bce0497ff0ff4db2f58a157d07069d2
StevenRX78 <110603111+StevenRX78@users.noreply.github.com>
2023-08-28 12:55:45 +0000

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,48 @@
+const QUOTES = [
+  {
+    quote: "For now they kill me with a living death",
+    author: "William Shakespeare",
+  },
+  {
+    quote: "Tired with all these, for restful death I cry",
+    author: "William Shakespeare",
+  },
+  {
+    quote: "Let me be boiled to death with melancholy",
+    author: "William Shakespeare",
+  },
+  {
+    quote: "For in that sleep of death what dreams may come",
+    author: "William Shakespeare",
+  },
+  {
+    quote: "Though Death be poor, it ends a mortal woe",
+    author: "William Shakespeare",
+  },
+  {
+    quote:
+      "I shall soon be laid in the quiet grave - thank God for the quiet grave",
+    author: "John Keats",
+  },
+  {
+    quote:
+      "Darkling I listen; and, for many a time I have been half in love with easeful Death",
+    author: "John Keats",
+  },
+  {
+    quote: "Death will come when thou art dead, soon, too soon",
+    author: "Percy Bysshe Shelley",
+  },
+  {
+    quote: "How wonderful is death! Death and his brother sleep",
+    author: "Percy Bysshe Shelley",
+  },
+  {
+    quote: "Death is the quiet haven of us all.",
+    author: "William Wordsworth",
+  },
+  {
+    quote: "And mighty poets in their misery dead.",
+    author: "William Wordsworth",
+  },
+];
